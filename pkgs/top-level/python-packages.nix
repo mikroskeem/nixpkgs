a5819474d19265d0e191fff1ac5d6ccf735f4e8d
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2019-10-22 00:28:54 +0200

    sphinxcontrib-applehelp: init at 1.0.1
    build dep of sphinx 2.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5005,6 +5005,8 @@ in {
 
   sphinx-testing = callPackage ../development/python-modules/sphinx-testing { };
 
+  sphinxcontrib-applehelp = callPackage ../development/python-modules/sphinxcontrib-applehelp {};
+
   sphinxcontrib-bibtex = callPackage ../development/python-modules/sphinxcontrib-bibtex {};
 
   sphinx-navtree = callPackage ../development/python-modules/sphinx-navtree {};
